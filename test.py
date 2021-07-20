e6800b985eb5abc0fd3d191c9fdc5ff23ef1b1fe
Ryan Johnson <rtjohnson1206@gmail.com>
2021-07-20 11:50:58 -0700

    modify test

--- test.py
+++ test.py
@@ -1,17 +1,2 @@
 
-def function(number, kwarg = 3):
-    """This is a function
-
-    Args:
-        number (integer): A number
-        kwarg (integer, optional): Another integer. Defaults to 3.
-
-    Raises:
-        TypeError: Typer Error
-
-    Returns:
-        [type]: [description]
-    """
-    if number < 2:
-        raise TypeError
-    return kwarg
+print("Hello, World!")
